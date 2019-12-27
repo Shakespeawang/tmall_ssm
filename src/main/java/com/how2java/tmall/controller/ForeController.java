e7245b40cf95febaf271f68171a74cb3a836d3a8
Shakespeawang <jiabaow3@163.com>
2019-12-27 16:36:35 +0800

    加入注册登录与前端显示

--- src/main/java/com/how2java/tmall/controller/ForeController.java
+++ src/main/java/com/how2java/tmall/controller/ForeController.java
@@ -2,12 +2,16 @@ package com.how2java.tmall.controller;
 
 
 import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.User;
 import com.how2java.tmall.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.HtmlUtils;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -37,6 +41,42 @@ public class ForeController {
         productService.fillByRow(cs);
         model.addAttribute("cs", cs);
         return "fore/home";
+    }
+    @RequestMapping("foreregister")
+    public String register(Model model,User user) {
+        String name =  user.getName();
+        name = HtmlUtils.htmlEscape(name);
+        user.setName(name);
+        boolean exist = userService.isExist(name);
+
+        if(exist){
+            String m ="用户名已经被使用,不能使用";
+            model.addAttribute("msg", m);
+            model.addAttribute("user", null);
+            return "fore/register";
+        }
+        userService.add(user);
+
+        return "redirect:registerSuccessPage";
+    }
+
+    @RequestMapping("forelogin")
+    public String login(@RequestParam("name") String name, @RequestParam("password") String password, Model model, HttpSession session) {
+        name = HtmlUtils.htmlEscape(name);
+        User user = userService.get(name,password);
+
+        if(null==user){
+            model.addAttribute("msg", "账号密码错误");
+            return "fore/login";
+        }
+        session.setAttribute("user", user);
+        return "redirect:forehome";
+    }
+
+    @RequestMapping("forelogout")
+    public String logout( HttpSession session) {
+        session.removeAttribute("user");
+        return "redirect:forehome";
     }
 
 }
